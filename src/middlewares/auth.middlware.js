716ea16c09426d7f4bbf0cd1b82e14c04b352b73
Siddhant <siddhant_010@outlook.com>
2024-06-25 12:42:13 +0530

    first commmit

---
+++ src/middlewares/auth.middlware.js
@@ -0,0 +1,33 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from '../models/user.model.js';
+
+export const verifyJWT = asyncHandler( async(req, _, next) => {
+
+    try {
+
+        // may be user is using phone so cookies does not have access to accessToken, so use another method
+        // whya accessToken is used?
+        const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        if( !token ) {
+            throw new ApiError(401, "Unauthorized Access")
+        }
+        
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id)
+        .select("-password -refreshToken");
+
+        if(!user) {
+            // discussion
+            throw new ApiError(401, "Invalid Access Token")
+        }
+        
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+    
+} )
---
+++ src/routes/video.route.js
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import {
+    deleteVideo,
+    getAllVideos,
+    getVideoById,
+    publishAVideo,
+    togglePublishStatus,
+    updateVideo,
+    getAllDBVideo
+} from "../controllers/video.controller.js"
+import {verifyJWT} from "../middlewares/auth.middlware.js"
+import {upload} from "../middlewares/multer.middleware.js"
+
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router.route("/get-videos").get(getAllVideos)
+router.route("/upload-video").post(
+    upload.fields([
+        {
+            name: "videoFile",
+            maxCount: 1,
+        },
+        {
+            name: "thumbnail",
+            maxCount: 1,
+        }
+    ]),
+    
+    publishAVideo
+);
+
+router.route("/home-videos").get(getAllDBVideo)
+    
+router
+    .route("/c/:videoId")
+    .get(getVideoById)
+    .delete(deleteVideo)
+    .patch(upload.single("thumbnail"), updateVideo);
+
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+
+export default router
